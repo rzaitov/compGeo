98763dbdd0c5d42d9092616ee16077380a848a53
Rustam Zaitov <r-zaitov@yandex.ru>
2021-01-28 13:05:57 +0300

    rewrite GeogrToView function

--- source/functions/stereonet_utils.py
+++ source/functions/stereonet_utils.py
@@ -1,44 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Pole import Pole as Pole
-from GeogrToView import GeogrToView
+from geogr_to_view import geogr_to_view
 from small_circle import small_circle
 from great_circle import great_circle
 from sph_to_cart import sph_to_cart
 from st_coord_line import eq_angle_stereonet_transform
 
+# some constants
 pi = np.pi
+east = pi / 2.0
+west = 3.0 * east
 
 def plot_eq_angle_stereonet(trdv, plgv, intrad):
-	# some constants
-	east = pi/2.0
-	west = 3.0*east
-	
-	# Plot stereonet reference circle
-	r = 1.0 # radius pf stereonet
-	TH = np.arange(0,360,1)*pi/180
-	X = r * np.cos(TH)
-	Y = r * np.sin(TH)
-	
 	# Make a larger figure
 	plt.rcParams['figure.figsize'] = [15, 7.5]
-	plt.plot(X,Y, 'k')
-	plt.axis ([-1, 1, -1, 1])
-	plt.axis ('equal')
-	plt.axis('off')
-	
+	plot_stnet_ref_circle()
+
 	# Number of small circles
 	n_circles = int(pi // intrad)
 
-	# small circles
-	# start at the North
-	trd = 0.0
-	plg = 0.0
-
-	# If view direction is not the default (trdv=0,plgv=90)
-	# transform line to view direction
-	if trdv != 0.0 and plgv != east:
-		trd, plg = GeogrToView(trd,plg,trdv,plgv)
+	# small circles start at the North
+	trd, plg = 0.0, 0.0
+	trd, plg = geogr_to_view(trd, plg, trdv, plgv)
 
 	# Plot small circles
 	for i in range(1, n_circles+1):
@@ -58,11 +41,7 @@ def plot_eq_angle_stereonet(trdv, plgv, intrad):
 			trd = east
 			plg = (i-n_circles)*intrad
 
-		# If view direction is not the default 
-		# (trdv = 0,plgv = 90)
-		# transform line to view direction
-		if trdv != 0.0 and plgv != east:
-			trd, plg = GeogrToView(trd,plg,trdv,plgv)
+		trd, plg = geogr_to_view(trd, plg, trdv, plgv)
 
 		pole_n, pole_e, pole_d = sph_to_cart(trd, plg)
 		NED_GC = great_circle(pole_n, pole_e, pole_d)
@@ -72,3 +51,15 @@ def plot_eq_angle_stereonet(trdv, plgv, intrad):
 		X, Y = eq_angle_stereonet_transform(NED_GC[0,:], NED_GC[1,:], NED_GC[2,:])
 
 		plt.plot(X, Y, color='gray', linewidth=0.5)
+
+def plot_stnet_ref_circle():
+	r = 1.0 # radius pf stereonet
+	TH = np.radians(np.arange(0,360,1))
+	X = r * np.cos(TH)
+	Y = r * np.sin(TH)
+
+	# Make a larger figure
+	plt.plot(X,Y, 'k')
+	plt.axis([-1, 1, -1, 1])
+	plt.axis('equal')
+	plt.axis('off')
--- source/functions/zero_two_pi.py
+++ source/functions/zero_two_pi.py
@@ -1,8 +1,8 @@
 import math
-h = 2 * math.pi
+two_pi = 2 * math.pi
 
 # This methog could accept number or numpy array
 # Here we exploit that integer division in Python rounds down (not a truncation as in other languages such as C or C++)
 # https://docs.python.org/3/library/operator.html#operator.floordiv
 def zero_two_pi(a):
-	return a - (a // h) * h
+	return a - (a // two_pi) * two_pi
