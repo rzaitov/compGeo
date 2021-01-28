4cca28010b368677eee5cf4bc743583fbc08af0c
Rustam Zaitov <r-zaitov@yandex.ru>
2021-01-28 15:28:42 +0300

    vectorize cart_to_sph and sph_to_cart

--- source/functions/cart_to_sph.py
+++ source/functions/cart_to_sph.py
@@ -1,5 +1,5 @@
-import math
-from ZeroTwoPi import ZeroTwoPi
+import numpy as np
+from zero_two_pi import zero_two_pi
 
 def cart_to_sph(cn, ce, cd):
 	'''
@@ -11,27 +11,41 @@ def cart_to_sph(cn, ce, cd):
 
 	NOTE: Trend and plunge are returned in radians
 
-	CartToSph uses function ZeroTwoPi
-
 	Python function translated from the Matlab function
 	CartToSph in Allmendinger et al. (2012)
 	'''
-	pi = math.pi
-	plg = math.asin(cd) # Eq. 4.13a
+	# convert scallar into 1d array https://numpy.org/doc/stable/reference/generated/numpy.atleast_1d.html
+	cn, ce, cd = np.atleast_1d(cn, ce, cd)
+	if cn.shape != ce.shape or ce.shape != cd.shape:
+		raise ValueError("shapes of arguments are incompatible")
+
+	pi = np.pi
+	pi_2 = pi / 2.0
+	plg = np.arcsin(cd) # Eq. 4.13a
+	trd = np.zeros(plg.shape)
 
 	# Trend: If north direction cosine is zero, trend
 	# is east or west. Choose which one by the sign of
 	# the east direction cosine
-	if cn == 0.0:
-		if ce < 0.0:
-			trd = 3.0/2.0*pi # Eq. 4.14d, trend is west
-		else:
-			trd = pi/2.0 # Eq. 4.14c, trend is east
-	else:
-		trd = math.atan(ce/cn) # Eq. 4.14a
-		if cn < 0.0:
-			trd = trd+pi # Eq. 4.14b
-		# Make sure trend is between 0 and 2*pi
-		trd = ZeroTwoPi(trd)
 
-	return trd, plg
+	mask = (cn == 0.0) * (ce <  0.0)
+	trd[mask] = 3.0 * pi_2 # Eq. 4.14d, trend is west
+
+	mask = (cn == 0.0) * (ce >= 0.0)
+	trd[mask] = pi_2      # Eq. 4.14c, trend is east
+
+	mask = (cn != 0.0)
+	trd[mask] = np.arctan(ce[mask]/cn[mask]) # Eq. 4.14a
+
+	mask = (cn < 0.0)
+	trd[mask] = trd[mask] + pi # Eq. 4.14b
+
+	# Make sure trend is between 0 and 2*pi
+	trd = zero_two_pi(trd)
+
+	if trd.shape[0] == 1:
+		return trd.item(), plg.item()
+	else:
+		return trd, plg
+
+
--- source/functions/sph_to_cart.py
+++ source/functions/sph_to_cart.py
@@ -1,9 +1,9 @@
-import math
+import numpy as np
 
 # Proposed API. There is no k parameter.
 # Here we follow python's name conventions for functions (all lower case with underscore)
 def sph_to_cart(trd, plg):
-    cn = math.cos(trd) * math.cos(plg)
-    ce = math.sin(trd) * math.cos(plg)
-    cd = math.sin(plg)
+    cn = np.cos(trd) * np.cos(plg)
+    ce = np.sin(trd) * np.cos(plg)
+    cd = np.sin(plg)
     return cn, ce, cd
