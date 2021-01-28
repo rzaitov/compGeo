914a6cc662d91e0b0f220b78a58c569779770d16
Rustam Zaitov <r-zaitov@yandex.ru>
2021-01-28 15:29:26 +0300

    unpack scalar if necessary

--- source/functions/lower_hemisphere.py
+++ source/functions/lower_hemisphere.py
@@ -6,4 +6,6 @@ def lower_hemisphere(trd, plg):
 	condition = plg < 0
 	trd = np.where(condition, zero_two_pi(trd + np.pi), trd)
 	plg = np.where(condition, -plg, plg)
+
+	if np.ndim(trd) == 0: return trd.item(), plg.item()
 	return trd, plg
