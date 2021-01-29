6d62b711618e141f05773acb6f4ab27212c659c5
Rustam Zaitov <r-zaitov@yandex.ru>
2021-01-29 21:35:57 +0300

    modernize ch5-3 notebook

--- source/functions/outcrop_trace.py
+++ source/functions/outcrop_trace.py
@@ -1,15 +1,19 @@
 import numpy as np
 
-def outcrop_trace(T, enu_point, XYZ):
-    # We just need the third row of ENU to SDP matrix
-    P_row = T[2, :]
+def outcrop_trace(T, enu_point, X, Y, Z):
+	# stack 2D arrays over third axis (-1 means last)
+	# https://numpy.org/doc/stable/reference/generated/numpy.stack.html
+	XYZ = np.stack([X, Y, Z], axis=-1)
 
-    # Estimate the P coordinate of the outcrop point p1
-    p = np.dot(P_row, enu_point)
+	# We just need the third row of ENU to SDP matrix
+	P_row = T[2, :]
 
-    # XYZ is (n, m, 3) cube of data
-    # np.dot will calculate sum product over last axis of DG and vector p_transform
-    # D[i, j] equals np.dot(XYZ[i,j,:], P_row)
-    DG = p - np.dot(XYZ, P_row)
+	# Estimate the P coordinate of the outcrop point p1
+	p = np.dot(P_row, enu_point)
 
-    return DG
+	# XYZ is (n, m, 3) cube of data
+	# np.dot will calculate sum product over last axis of DG and vector p_transform
+	# D[i, j] equals np.dot(XYZ[i,j,:], P_row)
+	DG = p - np.dot(XYZ, P_row)
+
+	return DG
